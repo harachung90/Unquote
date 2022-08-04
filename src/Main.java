d0cebe54fd91daa540bbe908b50c1fac7aa3467a
Hara <harachung90@gmail.com>
2022-08-04 19:02:45 +1000

    Second commit. Completed Game Logic Pt. 2. Step 1 - 21.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Question testQuestion = new Question(0, "Who invented the computer algorithm?", "Bill Gates", "Charles Babbage", "Ada Lovelace", "Leonardo DaVinci", 2);
+/*        Question testQuestion = new Question(0, "Who invented the computer algorithm?", "Bill Gates", "Charles Babbage", "Ada Lovelace", "Leonardo DaVinci", 2);
 
         System.out.println("The player’s guess is: " + testQuestion.playerAnswer);
         if (testQuestion.playerAnswer == -1) {
@@ -20,6 +20,27 @@ public class Main {
         System.out.println("A random number between 0 and 1 million: " + mainActivity.generateRandomNumber(1000000));
 
         System.out.println("Game over test 1 (should be 2 out of 3): " + mainActivity.getGameOverMessage(2, 3));
-        System.out.println("Game over test 2 (should be 4 out of 4): " + mainActivity.getGameOverMessage(4, 4));
+        System.out.println("Game over test 2 (should be 4 out of 4): " + mainActivity.getGameOverMessage(4, 4));*/
+
+        MainActivity mainActivity = new MainActivity();
+        mainActivity.startNewGame();
+
+        System.out.println("Questions remaining: " + mainActivity.totalQuestions);
+        Question currentQuestion = mainActivity.getCurrentQuestion();
+        printQuestion(currentQuestion);
+        // Play the game!
+        /*mainActivity.getCurrentQuestion();
+        printQuestion(currentQuestion);
+        mainActivity.onAnswerSubmission();*/
+        mainActivity.onAnswerSubmission();
+    }
+
+    static void printQuestion(Question question) {
+        System.out.println("Question: " + question.questionText);
+        System.out.println("Option 1: " + question.answer0);
+        System.out.println("Option 2: " + question.answer1);
+        System.out.println("Option 3: " + question.answer2);
+        System.out.println("Option 4: " + question.answer3);
+
     }
 }
--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,24 +1,91 @@
+import java.util.ArrayList;
+
 public class MainActivity {
 
-    // Add generateRandomNumber() here
-    int generateRandomNumber(int max) {
-        double randomDouble = Math.random();
-        randomDouble = (randomDouble * max);
-        int randomNum = (int)randomDouble;
-        return randomNum;
+    // TODO #1: add integer member variables here
+    int currentQuestionIndex;
+    int totalCorrect;
+    int totalQuestions;
+
+    // TODO #2: add ArrayList member variable here
+    ArrayList<Question> questions = new ArrayList<Question>();
+
+    // TODO #3 add startNewGame() here
+    void startNewGame() {
+        // Create three questions here
+        Question question1 = new Question(921238, "How tall is the Eiffel tower?", "1024 ft", "1063 ft", "1124 ft", "1163 ft", 1);
+
+        System.out.println("Question 1: " +  question1.questionText);
+        System.out.println("The answer is, " + question1.answer1);
+        question1.playerAnswer = 1;
+        System.out.println("The player chose: " + question1.playerAnswer);
+
+        Question question2 = new Question(107343, "Who invented the computer algorithm?", "Charles Babbage", "John Carmack", "Alan Turing", "Ada Lovelace", 3);
+        System.out.println("Question 2: " +  question2.questionText);
+        System.out.println("The answer is, " + question2.answer2);
+        question2.playerAnswer = 3;
+        System.out.println("The player chose: " + question2.playerAnswer);
+
+        Question question3 = new Question(748294, "What is the name for the patch of skin found on your elbow?", "Elbow Skin", "Fascia Elbora", "Wenis", "Todd", 2);
+        System.out.println("Question 3: " +  question3.questionText);
+        System.out.println("The answer is, " + question3.answer3);
+        question3.playerAnswer = 2;
+        System.out.println("The player chose: " + question3.playerAnswer);
+
+        //ArrayList<Question> questions = new ArrayList<Question>();
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+
+        totalCorrect = 0;
+        totalQuestions = questions.size();
+
+        Question firstQuestion = chooseNewQuestion();
+        // displayQuestion(firstQuestion);
+        // displayQuestionsRemaining(questions.size());
+    }
+    // TODO #4 add chooseNewQuestion() here
+    Question chooseNewQuestion() {
+        int randNum = generateRandomNumber(questions.size() - 1);
+        currentQuestionIndex = randNum;
+        return questions.get(currentQuestionIndex);
     }
 
-    // Add getGameOverMessage() here
+    // TODO #5 add getCurrentQuestion() here
+    Question getCurrentQuestion() {
+        return questions.get(currentQuestionIndex);
+    }
+    // TODO #6 add onAnswerSubmission() here
+    void onAnswerSubmission(){
+        Question currentQ = getCurrentQuestion();
+        if (currentQ.isCorrect()) {
+            totalCorrect += 1;
+            questions.remove(currentQ);
+        }
+        questions.remove(currentQuestionIndex);
+        // displayQuestionsRemaining(questions.size());
+        if (questions.size() == 0) {
+            System.out.println("Game over!");
+            startNewGame();
+            chooseNewQuestion();
+        } else {
+
+            // TODO: uncomment after implementing displayQuestion()
+            // displayQuestion(getCurrentQuestion());
+        }
+    }
+
+    int generateRandomNumber(int max) {
+        double randomNumber = Math.random();
+        double result = max * randomNumber;
+        return (int) result;
+    }
+
     String getGameOverMessage(int totalCorrect, int totalQuestions) {
         if (totalCorrect == totalQuestions) {
-            String winMessage = "You got all " + totalQuestions + " right! You won!";
-            //System.out.println(winMessage);
-            return winMessage;
+            return "You got all " + totalQuestions + " right! You won!";
         } else {
-            String loseMessage = "You got " + totalCorrect + " right out of " + totalQuestions + ". Better luck next time!";
-            //System.out.println(loseMessage);
-            return loseMessage;
+            return "You got " + totalCorrect + " right out of " + totalQuestions + ". Better luck next time!";
         }
-
     }
 }
